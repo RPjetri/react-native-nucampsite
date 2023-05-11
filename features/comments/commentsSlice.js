cd07da19aeb0ab3b77d72f49303f9c65213f81fb
Robert Pjetri <rpjetri91@gmail.com>
2023-05-10 19:00:04 -0500

    Favorites

--- features/comments/commentsSlice.js
+++ features/comments/commentsSlice.js
@@ -1,6 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { baseUrl } from '../../shared/baseUrl';
 
+
 export const fetchComments = createAsyncThunk(
     'comments/fetchComments',
     async () => {
@@ -15,10 +16,29 @@ export const fetchComments = createAsyncThunk(
     }
 );
 
+export const postComment = createAsyncThunk(
+    'comments/postComment',
+    async (payload, { dispatch, getState }) => {
+        setTimeout(() => {
+            const { comments } = getState();
+            payload = {
+                date: new Date(Date.now()).toISOString(),
+                id: comments.commentsArray.length
+            };
+
+            dispatch(addComment(payload));
+        }, 2000);
+    }
+);
+
 const commentsSlice = createSlice({
     name: 'comments',
     initialState: { isLoading: true, errMess: null, commentsArray: [] },
-    reducers: {},
+    reducers: {
+        addComment: (state, action) => {
+            state.commentsArray.push(action.payload);
+        }
+    },
     extraReducers: (builder) => {
         builder
             .addCase(fetchComments.pending, (state) => {
@@ -39,3 +59,5 @@ const commentsSlice = createSlice({
 });
 
 export const commentsReducer = commentsSlice.reducer;
+
+export const { addComment } = commentsSlice.actions; 
